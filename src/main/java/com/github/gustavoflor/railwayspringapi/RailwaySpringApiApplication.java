8a4aee255eb7c2ea19d693af0c9a05c6e47862b2
gustavo-flor <gustavo.flor@picpay.com>
2022-05-06 13:05:27 -0300

    :tada: Hello railway.app with Spring API

---
+++ src/main/java/com/github/gustavoflor/railwayspringapi/RailwaySpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.gustavoflor.railwayspringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwaySpringApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RailwaySpringApiApplication.class, args);
+    }
+
+}
